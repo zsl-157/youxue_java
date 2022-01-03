0d4f5b220fd50568ab08188b869b70736b0be85f
Administrator <1522192221@qq.com>
2022-01-03 21:37:32 +0800

    .

---
+++ src/main/java/com/youxue/project/shreal/ShrealApplication.java
@@ -0,0 +1,13 @@
+package com.youxue.project.shreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youxue/project/shreal/ShrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youxue.project.shreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
