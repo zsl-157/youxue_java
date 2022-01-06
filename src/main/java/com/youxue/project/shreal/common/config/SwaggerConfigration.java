d90b69eddd5dfc7510a115ba35aa173680942d02
Administrator <1522192221@qq.com>
2022-01-06 12:46:30 +0800

    增加swagger配置文件

---
+++ src/main/java/com/youxue/project/shreal/common/config/SwaggerConfigration.java
@@ -0,0 +1,36 @@
+package com.youxue.project.shreal.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigration {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                // api基础信息
+                // 选择那些路径和api会生成document
+                .select()
+                // 扫描展示api的路径包
+                .apis(RequestHandlerSelectors.basePackage("com.youxue.project.shreal.controller"))
+                // 对所有路径进行监控
+                .paths(PathSelectors.any())
+                // 构建
+                .build();
+
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("优学网").
+                description("在线学习平台").version("1.0").build();
+    }
+}
