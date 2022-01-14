fe1f24b1466ff05bcccf8944b77ebc94a78a2d83
Administrator <1522192221@qq.com>
2022-01-14 21:16:41 +0800

    增加logback配置文件

---
+++ src/main/java/com/youxue/project/shreal/common/Te.java
@@ -0,0 +1,14 @@
+package com.youxue.project.shreal.common;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Te {
+    private final static Logger logger = LoggerFactory.getLogger(Te.class);
+
+    public static void main(String[] args) {
+        logger.info("dsakfjkdsafjkdsafjkdasjfkjfdsakfdjkdsafjkdsalfjkjfdsk");
+    }
+}
--- src/main/java/com/youxue/project/shreal/controller/ApiTestController.java
+++ src/main/java/com/youxue/project/shreal/controller/ApiTestController.java
@@ -3,16 +3,20 @@ package com.youxue.project.shreal.controller;
 import com.youxue.project.shreal.service.RedisService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @Api(tags = "redis测试")
 @RestController
 @RequestMapping("/api")
 public class ApiTestController {
+    Logger logger = LoggerFactory.getLogger(ApiTestController.class);
     @Autowired
     private RedisService redisService;
     @Value("${spring.redis.key.prefix.userToken}")
@@ -21,13 +25,14 @@ public class ApiTestController {
     @GetMapping(value = "/v1")
     public String addRedisKey(){
         String key = "name";
-        String name = "张石磊";
+        String name = "zsl";
         try{
             redisService.setKey(key,name);
         }catch (Exception e){
             e.printStackTrace();
         }
-        return userToken+"设置成功";
+        logger.info("设置成功");
+        return userToken+"设置成功!";
     }
 
 }
